afa30803f88bba86773001d09037a97e36fa2986
Ibukun <ibk2k7@gmail.com>
2022-08-14 01:24:00 +0100

    see debug

--- DMA/settings.py
+++ DMA/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-v2c#od(i)7dq*-*56c=5^m_5i8$xg87x&w6=9)lskm7=xz(vuf"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
